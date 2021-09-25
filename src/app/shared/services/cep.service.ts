e23039893350892565b773426a4098fb3ea8f0c5
brunopbrito31 <brunopbrito31@hotmail.com>
2021-09-25 01:11:02 -0300

    servico-cep adicionado

---
+++ src/app/shared/services/cep.service.ts
@@ -0,0 +1,47 @@
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, throwError } from "rxjs";
+import { catchError, retry } from "rxjs/operators";
+import { Cep } from "src/app/models/cep";
+
+@Injectable({
+  providedIn: "root",
+})
+export class CepService {
+  url = "viacep.com.br/ws/";
+  urlEnd = "/json/";
+
+  // injetando o HttpClient
+  constructor(private httpClient: HttpClient) {}
+
+  // Headers
+  httpOptions = {
+    headers: new HttpHeaders({ "Content-Type": "application/json" }),
+  };
+
+  // Obtem um endereço pelo cep
+  getCep(cepProcurado: string): Observable<Cep> {
+    return this.httpClient
+      .get<Cep>(this.url + cepProcurado + this.urlEnd)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+  // Manipulação de erros
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = "";
+    if (error.error instanceof ErrorEvent) {
+      // Erro ocorreu no lado do client
+      errorMessage = error.error.message;
+    } else {
+      // Erro ocorreu no lado do servidor
+      errorMessage =
+        `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+}
